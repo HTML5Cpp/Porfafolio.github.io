128f39cc715fb575d8cb40e4a2b32ef70d6feb94
HTML5Cpp <68476621+HTML5Cpp@users.noreply.github.com>
2023-06-16 11:47:54 -0600

    Add files via upload

---
+++ JS/function.js
@@ -0,0 +1,27 @@
+let card = document.querySelectorAll('.card');
+//card.getAttribute('id');
+//console.log(card);
+
+
+card.forEach(carta => {
+    carta.addEventListener('click', () => {
+        let validador = carta.id;
+        console.log(validador);
+        
+        switch(validador){
+            case 'pro1':
+                window.location ='https://html5cpp.github.io/calculadora.github.io/';
+            break;
+            case 'pro2':
+                window.location ='https://proweb-animation.github.io/Stio-Web/index.html';
+            break;
+            case 'pro3':
+                window.location ='https://html5cpp.github.io/e-commerceevn-502/index.html';
+            break;
+            case 'pro4':
+                window.location ='https://html5cpp.github.io/PRK-Caps/';
+            break;
+        }
+    })
+})
+
